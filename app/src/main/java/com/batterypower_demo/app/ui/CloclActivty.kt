3e04d377345c079139364b57862a2c5cc940afd0
tushar <tushartripathi301997@gmail.com>
2023-03-12 22:29:15 +0530

    Clock Activity

---
+++ app/src/main/java/com/batterypower_demo/app/ui/CloclActivty.kt
@@ -0,0 +1,11 @@
+package com.batterypower_demo.app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CloclActivty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clocl_activty)
+    }
+}
